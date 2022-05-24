447792ff5d662ab6f16b917c18e716f08dff94eb
kabbo500 <kabbokazi3779@gmail.com>
2022-05-24 15:03:32 +0800

    get all cutomers reviews

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ async function run() {
             res.send(products);
         })
 
-        // app.get('/review', async (req, res) => {
-        //     const query = {};
-        //     const cursor = reviewCollection.find(query);
-        //     const reviews = await cursor.toArray(query);
-        //     res.send(reviews);
-        // })
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray(query);
+            res.send(reviews);
+        })
 
         // app.get('/product/:id', async (req, res) => {
         //     const id = req.params.id;
